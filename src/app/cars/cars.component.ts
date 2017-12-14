dc5c1f37195061e299ff30ad1d36e9805b703f80
newexod <dimapulenko@gmail.com>
2017-12-14 18:42:45 +0300

    Пайпы

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,6 +9,12 @@ export class CarsComponent {
   carName = '';
   addCarStatus = false;
   cars = ['Ford', 'BMW', 'Audi', 'Mazda', 'Lada', 'Bentley'];
+  dates = [
+    new Date(2015, 3, 4).toDateString(),
+    new Date(2011, 2, 5).toDateString(),
+    new Date(2017, 4, 6).toDateString(),
+    new Date(2000, 1, 7).toDateString()
+  ];
   
   constructor() { // Функция, которая вызывается, когда класс CarsComponent будет инициализироваться
   }
@@ -17,9 +23,5 @@ export class CarsComponent {
     this.addCarStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-
-  setBigCarText(car: string) {
-    return car.length > 4 ? true : false;
   }
 }
