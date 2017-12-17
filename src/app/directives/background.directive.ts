2eba20acb2f847c4d5fb592c22118d9ecf140685
newexod <dimapulenko@gmail.com>
2017-12-17 17:26:16 +0300

    Блок 4. Передача параметров внутрь директивы

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,60 +1,42 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
+// ПЕРЕДАЧА ПАРАМЕТРОВ ВНУТРЬ ДИРЕКТИВЫ
+import { 
+  Directive, 
+  ElementRef, 
+  OnInit, 
+  Renderer2, 
+  HostListener, 
+  HostBinding,
+  Input 
+} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]',
 })
 
 export class BackgroundDirective implements OnInit {
-  /*
-    'style.backgroundColor' - будет положена в переменную background
-  */
+  // Значение директивы appBackground попадёт в переменную hoverColor
+  @Input('appBackground') hoverColor; // по умолчанию будет зелёный цвет
+  @Input() defaultColor: string = 'transparent'; // по умолчанию будет прозрачный цвет
   @HostBinding('style.backgroundColor') background: string;
   @HostBinding('class') classStyle: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    
+    this.background = this.defaultColor;
   }
 
-  /* 
-    @HostListener - прослушка событий
-    'mouseevent' - ивэнт вхождения мыши на элемент
-    ['$event'] - принимает данный ивэнт
-    mouseEnter - название метода, который будет выполняться (callback)
-  */
   @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
-    console.log(event); // MouseEvent
+    console.log(event);
 
-    // 2 ВАРИАНТ 
-    this.background = 'lightblue';
+    this.background = this.hoverColor;
     this.classStyle = 'white-text';
-
-    /*
-      1 ВАРИАНТ
-      const {nativeElement} = this.element;
-      this.renderer.setStyle(nativeElement, 'background-color', 'lightblue');
-      this.renderer.addClass(nativeElement, 'white-text');
-    */
   }
 
-  /*
-    mouseleave - ивэнт выхода мыши из элемента
-  */
   @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
-    console.log(event); // MouseEvent
+    console.log(event);
 
-    // 2 ВАРИАНТ
-    this.background = 'transparent';
+    this.background = this.defaultColor;
     this.classStyle = 'black-text';
-
-    /*
-      1 ВАРИАНТ
-      const {nativeElement} = this.element;
-      this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
-      this.renderer.removeClass(nativeElement, 'white-text');
-    */
   }
 }
-
-// изменение директив при событии
