2c215d619c905e92abbd156700cdc4a82e64ba8d
newexod <dimapulenko@gmail.com>
2017-12-17 14:23:42 +0300

    Блок 4. Использование Renderer

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,19 +1,26 @@
-// ng g d backgroundDirective --spec=false
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appBackground]', // [] - указывает, что директиву нужно будет искать по аттрибуту в html
+  selector: '[appBackground]',
 })
 
 export class BackgroundDirective implements OnInit {
-  /* 
-    constructor - обычно используется для инъекций каких-то элементов
-    приватная переменная element, у которой тип ElementRef
-  */
-  constructor(private element: ElementRef) {}
+  // Renderer2 не устарел в отличии от Renderer
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    console.log(this.element); // element является обычной DOM нодой, т.к. является типом ElementRef
     this.element.nativeElement.style.backgroundColor = 'red';
+
+    /* 
+      Не всегда мы имеем доступ к DOM дереву (мобильное приложение, к примеру)
+      Таким образом там не будет nativeElement
+      На смену nativeElement приходит Renderer2
+      ХОРОШИМ ТОНОМ ЯВЛЯЕТСЯ ИСПОЛЬЗОВАНИЕ RENDERER!!!!
+    */
+    const {nativeElement} = this.element;
+    this.renderer.setStyle(nativeElement, 'background-color', 'lightblue');
+
+    // берется из app.component.css
+    this.renderer.addClass(nativeElement, 'white-text')
   }
 }
