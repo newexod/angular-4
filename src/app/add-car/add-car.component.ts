e316a28fa6472d51d098f9d3109a2fdb4bcbf277
newexod <dimapulenko@gmail.com>
2017-12-17 03:05:08 +0300

    Передача HTML-кода компоненту

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -16,35 +16,19 @@ export class AddCarComponent implements OnInit {
 
   @Output('onAddCar') carEmitter = new EventEmitter<{name: string, year: number}>();
 
-  // 2 СПОСОБ
-  @ViewChild('carYearInput') carYearInput: ElementRef; // обращаемся к референции 'carYearInput' и заносим её в переменную carYearInput, которая является типом ElementRef
+  @ViewChild('carYearInput') carYearInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  // 1 СПОСОБ
-  // addCar(carNameElement: HTMLInputElement) {
-  //   console.dir(carNameElement.value); // <input _ngcontent-c2 class="form-control" type="text">
-
-  //   this.carEmitter.emit({
-  //     name: carNameElement.value,
-  //     year: this.carYear
-  //   });
-
-  //   carNameElement.value = '';
-  //   this.carYear = 2017;
-  // }
-
-
-  // 2 СПОСОБ
   addCar(carNameElement: HTMLInputElement) {
-    console.log(this.carYearInput); // ElementRef - специальный ангуляр элемент у которого есть nativeElement, который является обычным html элементом (onclick, onfocus, value ...)
+    console.log(this.carYearInput);
 
     this.carEmitter.emit({
       name: carNameElement.value,
-      year: +this.carYearInput.nativeElement.value // + инкрементирует из строки в число
+      year: +this.carYearInput.nativeElement.value
     });
 
     carNameElement.value = '';
--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,5 +7,14 @@ import { Component, Input } from '@angular/core';
 })
 export class CarComponent {
   @Input('carItem') car: {name: string, age: number};
-  private carItem = false;
+
+  // ПОЛУЧЕНИЕ ЛОКАЛЬНОЙ РЕФЕРЕНЦИИ К ЭЛЕМЕНТУ не через @ViewChild, a через @ContentChild
+  // @ContentChild - это ТОЛЬКО то что находится внутри <ng-content></ng-content> в car.component.html
+  // @ViewChild - это ВСЁ что находится в car.component.html
+  @ContentChild('carHeading') carHeading: ElementRef;
+
+  // ngAfterViewInit будет вызван в том случае, если html уже занесён в DOM дерево
+  ngAfterViewInit() {
+    console.log(this.carHeading); // трижды вызовется console.log. в this.carHeading будет: ElementRef {nativeElement: h4.list-group-item-heading}
+  }
 }
