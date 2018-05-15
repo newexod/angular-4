49e6834267dc3a16d6c7b3395bfadb7849f04216
newexod <dimapulenko@gmail.com>
2018-05-15 15:58:42 +0300

    Блок 7. Формы. R Создание формы.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   answers = [
     {
@@ -17,5 +18,21 @@ export class AppComponent {
       text: 'Нет'
     }
   ];
+
+  // Экземпляр формы, внутри компонента
+  form: FormGroup;
+  // Инициализация формы (указать, как она связана с шаблоном, какие поля в ней есть)
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      pass: new FormControl(''),
+      country: new FormControl('ru'),
+      answer: new FormControl('No')
+    });
+  }
+
+  onSubmit() {
+    console.log('wow', this.form);
+  }
 
 }
