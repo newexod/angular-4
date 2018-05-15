dddf49d7aae8a2829ed0314bdd3afd3f6b3cbb7c
newexod <dimapulenko@gmail.com>
2018-05-15 15:17:08 +0300

    Блок 7. Формы. Реактивный подход.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +6,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('form') form: NgForm;
 
   answers = [
     {
@@ -19,37 +17,5 @@ export class AppComponent {
       text: 'Нет'
     }
   ];
-
-  defaultAnswer = 'No';
-  defaultCountry = 'ru';
-
-  formData = {};
-  isSubmited = false;
-
-  submitForm() {
-    this.isSubmited = true;
-    this.formData = this.form.value;
-    this.form.reset(); // очистить форму
-  }
-
-  addRandomEmail() {
-    const randomEmail = 'dimapulenko@gmail.com';
-    // Если изначально заполнить всю форму валидным значением, а после нажать на кнопку "Добавить случайный email", то <input> для email заполнится случайным значением, а остальные поля будут пустыми
-    // this.form.setValue({
-    //   user: {
-    //     pass: '',
-    //     email: randomEmail
-    //   },
-    //   country: '',
-    //   answer: ''
-    // });
-
-    // Поэтому лучше использовать не setValue, a patchValue - динамически изменит конкретное поле
-    this.form.form.patchValue({
-      user: {
-        email: randomEmail
-      }
-    });
-  }
 
 }
