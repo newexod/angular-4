898bf68045b70d5322c5e141516d2875a7daeb0e
newexod <dimapulenko@gmail.com>
2018-05-15 13:51:44 +0300

    Блок 7. Формы. TD Валидация формы 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,20 +22,5 @@ export class AppComponent {
   submitForm(form: NgForm) {
     console.log('wow', form);
   }
-
-  // до ввода в <input>
-  // ng-untouched - означает, что данный <input> ещё не трогали и ничего в нём не вписывали
-  // ng-pristine - означает, что сейчас данный <input> пустой
-  // ng-invalid - означает, что данный <input> невалидный
-
-  // после добавления символа в <input> 
-  // ng-untouched
-  // ng-invalid
-  // ng-dirty
-
-  // если убрать focus с <input>
-  // ng-invalid
-  // ng-dirty
-  // ng-touched - означает, что убрали focus с данного <input> и при этом он до сих пор невалидный
   
 }
