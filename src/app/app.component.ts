45a64b069b21271f95cdd3c893860aa5d2c4403c
newexod <dimapulenko@gmail.com>
2018-05-15 19:21:11 +0300

    Блок 8. HTTP.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { reject } from 'q';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  answers = [
+  cars = [
     {
-      type: 'Yes',
-      text: 'Да'
-    },
-    {
-      type: 'No',
-      text: 'Нет'
+      name: 'Ford',
+      color: 'white',
+      id: 1
     }
   ];
 
-  charsCount = 4;
-
-  form: FormGroup;
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email], this.checkForEmail),
-        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)])
-      }),
-      country: new FormControl('ru'),
-      answer: new FormControl('No')
-    });
-  }
-
-  onSubmit() {
-    console.log('wow', this.form);
-  }
-
-  checkForLength(control: FormControl) {
-    if (control.value.length <= this.charsCount) {
-      return {
-        'lengthError': true
-      };
-    }
-
-    return null;
-  }
-
-  // Асинхронный валидатор
-  // К примеру, заполняем поле email
-  // Нужно спросить сервер: "Есть ли пользователь с таким email?"
-  // Если пользователь с таким email имеется нужно уведомить пользователя о том, что такой email занят. Нужно использовать другой email
-  // Асинхронный валидатор будет ходить на сервер, спрашивать сервер есть ли такой email в базе данных и возвращать ответ в асинхронном режиме
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@mail.ru') {
-          resolve({
-            'emailIsUsed': true
-          });
-        } else {
-          resolve(null);
-        }
-      }, 3000);
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
