07392f2622fb06fa5677ed52e75568a3534c465d
newexod <dimapulenko@gmail.com>
2018-05-16 15:34:28 +0300

    Блок 9. Роутинг.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CarsService } from './cars.service';
-
-
-interface Cars {
-  name: string;
-  color: string;
-  id: number;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  cars: any;
-  carName: string = '';
-  colors = ['red', 'blue', 'green', 'pink', 'yellow', 'grey'];
-  appTitle;
-
-  constructor(private carsService: CarsService) {}
-
-  ngOnInit() {
-    this.appTitle = this.carsService.getAppTitle();
-  }
-
-  loadCars() {
-    this.cars = this.carsService.getCars();
-  }
-
-  addCar() {
-    this.carsService
-      .addCar(this.carName)
-      .subscribe((car: Cars) => {
-        this.cars.push(car);
-      });
-    this.carName = '';
-  }
-
-  getRandomColor() {
-    const num = Math.round(Math.random() * (this.colors.length - 1));
-    return this.colors[num];
-  }
-
-  setNewColor(car: Cars) {
-    this.carsService.changeColor(car, this.getRandomColor())
-      .subscribe((data) => {
-        console.log(data)
-      });
-  }
-
-  deleteCar(car: Cars) {
-    this.carsService.deleteCar(car)
-      .subscribe((data) => {
-        this.cars = this.cars.filter(c => c.id !== car.id);
-      });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 import { CarsService } from './cars.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
