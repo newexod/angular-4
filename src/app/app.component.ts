1f2c8aeffc2877238c1e403e763296592fb6ecac
newexod <dimapulenko@gmail.com>
2018-05-16 15:16:56 +0300

    Блок 8. HTTP. Async пайп.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarsService } from './cars.service';
 
 
@@ -13,25 +13,21 @@ interface Cars {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  cars: Cars[] = [];
+  cars: any;
   carName: string = '';
   colors = ['red', 'blue', 'green', 'pink', 'yellow', 'grey'];
+  appTitle;
 
   constructor(private carsService: CarsService) {}
 
+  ngOnInit() {
+    this.appTitle = this.carsService.getAppTitle();
+  }
+
   loadCars() {
-    this.carsService
-      .getCars()
-      .subscribe(
-        (cars: Cars[]) => {
-          this.cars = cars;
-        },
-        (error) => {
-          alert(error);
-        }
-      );
+    this.cars = this.carsService.getCars();
   }
 
   addCar() {
