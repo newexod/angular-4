728618b5e213070058f1ecdad421beb3c6dc9f3b
newexod <dimapulenko@gmail.com>
2018-05-21 21:14:04 +0300

    Блок 11. Анимации.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { HeaderComponent } from './header/header.component';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { SharedModule } from './shared/shared.module';
 
 
-// Модуль === NgModule - это сущность, которая объединяет в себе все другие сущности
 @NgModule({
-  declarations: [ // может хранить пайпы, компоненты, директивы
-    AppComponent,
-    HomePageComponent,
-    HeaderComponent
+  declarations: [
+    AppComponent
   ],
-  imports: [ // то, что модуль должен в себя импортировать, те модули, которые ему нужны для работы  
-    BrowserModule, // модуль, который предоставляет фичи, которые работают в браузере
-    FormsModule,
-    AppRoutingModule,
-    SharedModule    
+  imports: [  
+    BrowserModule,
+    FormsModule  
   ],
-  providers: [], // массив, где регистрируем сервисы
-  bootstrap: [AppComponent] // то, что необходимо запустить при старте приложения
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-// Общий модуль, который будет загружать общие сущности (например пайпы или директивы). В данном случае директиву, которая будет использоваться во всех модулях
-import { NgModule } from '@angular/core';
-import { ColorDirective } from './color.directive';
-
-@NgModule({
-  declarations: [ColorDirective],
-  exports: [ColorDirective]
-})
-export class SharedModule {}
