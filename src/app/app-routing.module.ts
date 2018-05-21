14525fa5d18ba965c3ca95e45a4cc653844a1452
newexod <dimapulenko@gmail.com>
2018-05-21 21:03:04 +0300

    Блок 10. Модули. Предзагрузка модулей.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'cars', loadChildren: './cars-page/cars.module#CarsModule' } // Если был переход на роут CarsPageComponent, то загрузить CarsModule
-  // Если необходимо загружать Guard с ленивой загрузкой, то используется canLoad: [] вместо canActivate: []
+  { path: 'cars', loadChildren: './cars-page/cars.module#CarsModule' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
