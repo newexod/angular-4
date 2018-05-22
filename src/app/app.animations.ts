d0497ca884ccf1d69c1e00cd72e85a6a38844f4d
newexod <dimapulenko@gmail.com>
2018-05-22 16:01:46 +0300

    Блок 11. Анимации. Детальное управление анимацией.

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,17 +1,41 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, group, keyframes } from '@angular/animations';
 
 export const divTrigger = trigger('divTrigger', [
-  // void => * , где * - означает, что будем использовать любой переход состояния
-  transition(':enter', [ // :enter === void => *
+  /**
+   * Когда заходим в функцию transition, определяем, что будет state(:enter) (появление элемента в DOM)
+   * Этому элементу задаём его исходные размеры, чтобы стартовать от них
+   * Определяем новую группу
+   * В группе - это те же анимации, которые начинаются в одно и то же время, но длятся по-разному
+   * 1 анимация будет длиться 3сек - будет менять размер <div>
+   * 2 анимация, которая стартует одновременно с 1 анимацией - будет менять цвет элемента, но за 6сек. 
+   * За эти 6сек цвет меняется 3 раза (2сек - blue, затем 2сек - yellow, затем 2сек - pink)
+   * Группа будет длится 6сек
+   * И только потом за 1сек <div> вернётся в исходное состояние
+   */
+
+   /**
+    * keyframes - ф-ция предназначена для декомпозиции времени анимации, для использования разных свойств
+    * Передаётся в animate вместо style
+    */
+  transition(':enter', [
     style({
-      opacity: 0
+      width: '*',
+      height: '*'
     }),
-    animate(1500, style({
-      opacity: 1
-    }))
+    group([ // объединяет в группу некоторые анимации
+      animate(3000, style({
+        width: '200px',
+        height: '200px'
+      })),
+      animate(6000, keyframes([
+        style({backgroundColor: 'blue'}),
+        style({backgroundColor: 'yellow'}),
+        style({backgroundColor: 'pink'})
+      ]))
+    ]),
+    animate(1000)
   ]),
-  // * => void
-  transition(':leave', animate(1500, style({ // :leave === * => void
+  transition(':leave', animate(1500, style({
     opacity: 0
   })))
 ]);
