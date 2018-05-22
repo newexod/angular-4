22493f8266127bccc31c314042e80a9f46ce3f8a
newexod <dimapulenko@gmail.com>
2018-05-22 17:25:51 +0300

    Блок 12. Юнит тесты.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
-import { divTrigger, changeWidthTrigger } from './app.animations';
-import { AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [divTrigger, changeWidthTrigger]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = false;
-
-  onAnimationStart(event: AnimationEvent) {
-    console.log('Start: ', event);
-  }
-
-  onAnimationDone(event: AnimationEvent) {
-    console.log('Done: ', event);
-  }
+  
 }
